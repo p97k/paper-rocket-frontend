8d5a9a0791f2a3e2ab675dbd72ec1a4e8fa77ae4
parham <parham.khoshbin@digikala.com>
2024-04-01 18:56:55 +0330

    chore: handle env urls

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,6 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8080'
-export const WEBSOCKET_URL = process.env.NEXT_PUBLIC_WEBSOCKET_URL || 'ws://127.0.0.1:8080'
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://paperrocket-zbapz2lp.b4a.run'
+export const WEBSOCKET_URL = process.env.NEXT_PUBLIC_WEBSOCKET_URL || 'ws://paperrocket-zbapz2lp.b4a.run'
+
+//LOCAL URL (COMMENT URLS ABOVE)
+// export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8080'
+// export const WEBSOCKET_URL = process.env.NEXT_PUBLIC_WEBSOCKET_URL || 'ws://127.0.0.1:8080'
